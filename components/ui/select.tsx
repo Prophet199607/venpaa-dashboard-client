4a8ccc71c43ad286ac514b51d85b839768497802
Thamalpath <thamalpathsathimantha3@gmail.com>
2025-09-25 10:02:31 +0530

    Update SelectContent

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -8,43 +8,72 @@ export function Select({ children, ...props }: SelectPrimitive.SelectProps) {
   return <SelectPrimitive.Root {...props}>{children}</SelectPrimitive.Root>;
 }
 
-export const SelectTrigger = React.forwardRef<HTMLButtonElement, SelectPrimitive.SelectTriggerProps>(
-  ({ className, children, ...props }, ref) => (
-    <SelectPrimitive.Trigger
-      ref={ref}
-      className={cn("input flex items-center justify-between", className)}
-      {...props}
-    >
-      {children}
-      <SelectPrimitive.Icon><ChevronDown size={16} /></SelectPrimitive.Icon>
-    </SelectPrimitive.Trigger>
-  )
-);
+export const SelectTrigger = React.forwardRef<
+  HTMLButtonElement,
+  SelectPrimitive.SelectTriggerProps
+>(({ className, children, ...props }, ref) => (
+  <SelectPrimitive.Trigger
+    ref={ref}
+    className={cn("input flex items-center justify-between", className)}
+    {...props}
+  >
+    {children}
+    <SelectPrimitive.Icon>
+      <ChevronDown size={16} />
+    </SelectPrimitive.Icon>
+  </SelectPrimitive.Trigger>
+));
 SelectTrigger.displayName = "SelectTrigger";
 
-export const SelectContent = React.forwardRef<HTMLDivElement, SelectPrimitive.SelectContentProps>(
-  ({ className, children, ...props }, ref) => (
-    <SelectPrimitive.Content ref={ref} className={cn("z-50 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900", className)} {...props}>
-      <SelectPrimitive.ScrollUpButton className="flex items-center justify-center h-8"><ChevronUp size={16} /></SelectPrimitive.ScrollUpButton>
-      <SelectPrimitive.Viewport className="p-1">{children}</SelectPrimitive.Viewport>
-      <SelectPrimitive.ScrollDownButton className="flex items-center justify-center h-8"><ChevronDown size={16} /></SelectPrimitive.ScrollDownButton>
-    </SelectPrimitive.Content>
-  )
-);
-SelectContent.displayName = "SelectContent";
-
-export const SelectItem = React.forwardRef<HTMLDivElement, SelectPrimitive.SelectItemProps>(
-  ({ className, children, ...props }, ref) => (
-    <SelectPrimitive.Item
+export const SelectContent = React.forwardRef<
+  HTMLDivElement,
+  SelectPrimitive.SelectContentProps
+>(({ className, children, ...props }, ref) => (
+  <SelectPrimitive.Portal>
+    <SelectPrimitive.Content
       ref={ref}
-      className={cn("relative flex w-full cursor-default select-none items-center rounded-lg py-2 pl-8 pr-3 text-sm outline-none hover:bg-neutral-100 dark:hover:bg-neutral-800", className)}
+      position="popper"
+      sideOffset={4}
+      className={cn(
+        "z-50 w-[var(--radix-select-trigger-width)] rounded-md border border-neutral-200 bg-white dark:border-neutral-700 dark:bg-neutral-900 shadow-md",
+        className
+      )}
       {...props}
     >
-      <span className="absolute left-2 inline-flex w-4"><SelectPrimitive.ItemIndicator><Check size={16} /></SelectPrimitive.ItemIndicator></span>
-      <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
-    </SelectPrimitive.Item>
-  )
-);
+      <SelectPrimitive.ScrollUpButton className="flex items-center justify-center h-6">
+        <ChevronUp size={16} />
+      </SelectPrimitive.ScrollUpButton>
+      <SelectPrimitive.Viewport className="p-1">
+        {children}
+      </SelectPrimitive.Viewport>
+      <SelectPrimitive.ScrollDownButton className="flex items-center justify-center h-6">
+        <ChevronDown size={16} />
+      </SelectPrimitive.ScrollDownButton>
+    </SelectPrimitive.Content>
+  </SelectPrimitive.Portal>
+));
+SelectContent.displayName = "SelectContent";
+
+export const SelectItem = React.forwardRef<
+  HTMLDivElement,
+  SelectPrimitive.SelectItemProps
+>(({ className, children, ...props }, ref) => (
+  <SelectPrimitive.Item
+    ref={ref}
+    className={cn(
+      "relative flex w-full cursor-default select-none items-center rounded-lg py-2 pl-8 pr-3 text-sm outline-none hover:bg-neutral-100 dark:hover:bg-neutral-800",
+      className
+    )}
+    {...props}
+  >
+    <span className="absolute left-2 inline-flex w-4">
+      <SelectPrimitive.ItemIndicator>
+        <Check size={16} />
+      </SelectPrimitive.ItemIndicator>
+    </span>
+    <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
+  </SelectPrimitive.Item>
+));
 SelectItem.displayName = "SelectItem";
 
 export const SelectValue = SelectPrimitive.Value;
