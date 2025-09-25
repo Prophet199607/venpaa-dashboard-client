f452361e1579760283283faaeb4efd5882cffb13
Pasindu Priyankara <pasinduxx@hotmail.com>
2025-09-25 17:13:02 +0530

    Add environment variable configuration to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: { typedRoutes: true },
-  reactStrictMode: true
+  reactStrictMode: true,
+  env: {
+    PORT: process.env.PORT || '5005'
+  }
 };
 export default nextConfig;
