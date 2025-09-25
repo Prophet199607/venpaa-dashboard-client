101bb820a3b993d4313a97f9dd1231071b1112e1
Pasindu Priyankara <pasinduxx@hotmail.com>
2025-09-25 19:11:56 +0530

    Add NotFound component and update next.config.js for image domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
+  images: {
+    domains: []
+  },
   env: {
     PORT: process.env.PORT || '5005'
   }
