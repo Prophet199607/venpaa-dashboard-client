bed61279aaa1ae931e8a4db89119101030e7deee
Thamalpath <thamalpathsathimantha3@gmail.com>
2025-10-07 16:06:29 +0530

    Next js configuration modified

--- next.config.js
+++ next.config.js
@@ -1,10 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: []
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/storage/**",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/storage/**",
+      },
+      // For production:
+      // {
+      //   protocol: 'https',
+      //   hostname: 'your-production-domain.com',
+      //   pathname: '/storage/**',
+      // },
+    ],
   },
   env: {
-    PORT: 5005
-  }
+    PORT: 5005,
+  },
 };
 export default nextConfig;
