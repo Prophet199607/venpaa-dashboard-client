faba1c269f8d177bc5f4cde5a6fbfb8e2e545d1d
Thamalpath <thamalpathsathimantha3@gmail.com>
2025-10-07 16:07:48 +0530

    Base URL updated with .env

--- utils/api.ts
+++ utils/api.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const authApi = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1",
+  baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}/v1`,
   headers: {
     "Content-Type": "application/json",
   },
