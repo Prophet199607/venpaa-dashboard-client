6b45a7a0bc8906cb0b7e126d5ee278c21a67a81f
Thamalpath <thamalpathsathimantha3@gmail.com>
2025-10-11 10:14:50 +0530

    Remove unused Label import from LocationDialog component

--- components/model/LocationDialog.tsx
+++ components/model/LocationDialog.tsx
@@ -8,7 +8,6 @@ import { useRouter } from "next/navigation";
 import Loader from "@/components/ui/loader";
 import { useToast } from "@/hooks/use-toast";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { Checkbox } from "@/components/ui/checkbox";
