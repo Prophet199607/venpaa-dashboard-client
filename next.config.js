98fcf3f55ce3054a822c5bbb5ba947e7ac4e2d96
Pasindu Priyankara <pasinduxx@hotmail.com>
2025-10-21 11:47:01 +0530

    refactor: clean up next.config.js formatting and remove commented production config

--- next.config.js
+++ next.config.js
@@ -3,27 +3,26 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8000",
-        pathname: "/storage/**",
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/storage/**'
       },
       {
-        protocol: "http",
-        hostname: "127.0.0.1",
-        port: "8000",
-        pathname: "/storage/**",
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '8000',
+        pathname: '/storage/**'
       },
-      // For production:
-      // {
-      //   protocol: 'https',
-      //   hostname: 'your-production-domain.com',
-      //   pathname: '/storage/**',
-      // },
-    ],
+      {
+        protocol: 'https',
+        hostname: 'venpaaapi.onimtaitsl.com',
+        pathname: '/storage/**'
+      }
+    ]
   },
   env: {
-    PORT: 5005,
-  },
+    PORT: 5005
+  }
 };
 export default nextConfig;
