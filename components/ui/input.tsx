e6d9f9f8b4384a98140d4a4c0d1c9cc96d6f9a4d
Thamalpath <thamalpathsathimantha3@gmail.com>
2025-10-25 12:17:17 +0530

    Handle border destructive

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,11 +1,16 @@
 import * as React from "react";
 import { cn } from "@/utils/cn";
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 export const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, ...props }, ref) => (
-    <input ref={ref} className={cn("input", className)} {...props} />
+    <input
+      ref={ref}
+      className={cn("input aria-[invalid=true]:border-destructive", className)}
+      {...props}
+    />
   )
 );
 Input.displayName = "Input";
