61af61e28c349514ea688f94f1cecaad50511c13
Thamalpath <thamalpathsathimantha3@gmail.com>
2026-06-03 11:55:11 +0530

    Add ID encoding/decoding utility functions with base64 and salt

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+const SALT = "vpd";
+
+export function encodeId(id: number | string): string {
+  return btoa(`${SALT}${id}`).replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, "");
+}
+
+export function decodeId(hash: string): string {
+  const base64 = hash.replace(/-/g, "+").replace(/_/g, "/");
+  return atob(base64).replace(SALT, "");
+}
